e8c50fb6f25fb0ed70700c7fab0ef0178e494964
Lars Tveito <larstvei@ifi.uio.no>
2015-09-16 17:18:50 +0200

    Updated for maude 2.7

--- maude.rb
+++ maude.rb
@@ -1,16 +1,17 @@
 class Maude < Formula
   homepage "http://maude.cs.illinois.edu"
-  url "http://maude.cs.illinois.edu/w/images/6/61/Maude-2.6.tar.gz"
-  sha256 "9c068f2c8253a6ea9bcff31f533c33d400376f84f989e082e1f77d21036234d4"
+  version "2.7"
+  url "http://maude.cs.illinois.edu/w/images/8/81/Maude27-osx.zip"
+  sha256 "8fefa4e313dd13e40957bf84086032356137c9c38ee2a9ecac381a6dfc986173"
 
   def install
-    bin.install "linear.maude"
-    bin.install "machine-int.maude"
-    bin.install "metaInterpreter.maude"
-    bin.install "model-checker.maude"
-    bin.install "prelude.maude"
-    bin.install "socket.maude"
-    bin.install "term-order.maude"
-    bin.install "maude.intelDarwin" => "maude"
+    bin.install "maude27-osx/linear.maude"
+    bin.install "maude27-osx/machine-int.maude"
+    bin.install "maude27-osx/metaInterpreter.maude"
+    bin.install "maude27-osx/model-checker.maude"
+    bin.install "maude27-osx/prelude.maude"
+    bin.install "maude27-osx/socket.maude"
+    bin.install "maude27-osx/term-order.maude"
+    bin.install "maude27-osx/maude.darwin64" => "maude"
   end
 end
