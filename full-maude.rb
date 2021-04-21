5cc9826636e6f99be29cdd54db8becc911e4cc35
Marius Lichtblau <marius@lichtblau.io>
2021-04-21 11:44:26 +0200

    Bump version to 3.1
    Update sha256 checksums
    
    Update Paths

--- full-maude.rb
+++ full-maude.rb
@@ -1,11 +1,11 @@
 class FullMaude < Formula
   homepage "http://maude.cs.illinois.edu"
-  version "2.7"
-  url "http://maude.cs.illinois.edu/w/images/c/ce/Full-maude-27.zip"
-  sha256 "965ea5410224e00846e0e6a2872edd861fe580ede9507a462057ed0300919396"
+  version "3.1"
+  url "http://maude.cs.illinois.edu/w/images/0/0a/Full-Maude-3.1.zip"
+  sha256 "8b13af02c6243116c2ef9592622ecaa06d05dbe1dd6b1e595551ff33855948f2"
   depends_on "maude"
 
   def install
-    bin.install "full-maude.maude"
+    bin.install "full-maude31.maude"
   end
 end
--- maude.rb
+++ maude.rb
@@ -1,17 +1,20 @@
 class Maude < Formula
   homepage "http://maude.cs.illinois.edu"
-  version "2.7"
-  url "http://maude.cs.illinois.edu/w/images/8/81/Maude27-osx.zip"
-  sha256 "8fefa4e313dd13e40957bf84086032356137c9c38ee2a9ecac381a6dfc986173"
+  version "3.1"
+  url "http://maude.cs.illinois.edu/w/images/9/93/Maude-3.1-macos.zip"
+  sha256 "2d1ba91beba27c65ae0e72b8a0596962cd2f2d008ffdfab168a5aa9c3dc6320d"
 
   def install
-    bin.install "maude27-osx/linear.maude"
-    bin.install "maude27-osx/machine-int.maude"
-    bin.install "maude27-osx/metaInterpreter.maude"
-    bin.install "maude27-osx/model-checker.maude"
-    bin.install "maude27-osx/prelude.maude"
-    bin.install "maude27-osx/socket.maude"
-    bin.install "maude27-osx/term-order.maude"
-    bin.install "maude27-osx/maude.darwin64" => "maude"
+    bin.install "file.maude"
+    bin.install "linear.maude"
+    bin.install "machine-int.maude"
+    bin.install "metaInterpreter.maude"
+    bin.install "model-checker.maude"
+    bin.install "prelude.maude"
+    bin.install "process.maude"
+    bin.install "smt.maude"
+    bin.install "socket.maude"
+    bin.install "term-order.maude"
+    bin.install "maude.darwin64" => "maude"
   end
 end
